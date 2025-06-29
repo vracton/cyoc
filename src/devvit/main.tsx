a6eabe3dac9672d3ca8bafe99d2578e9fc931f3c
vracto <115945845+vracton@users.noreply.github.com>
2025-06-29 16:05:58 -0500

    Fix Devvit rendering and API errors

--- src/devvit/main.tsx
+++ src/devvit/main.tsx
@@ -61,15 +61,6 @@ export const Preview: Devvit.BlockComponent<{ text?: string }> = ({ text = 'Load
   return (
     <zstack width={'100%'} height={'100%'} alignment="center middle">
       <vstack width={'100%'} height={'100%'} alignment="center middle">
-        <image
-          url="loading.gif"
-          description="Loading..."
-          height={'140px'}
-          width={'140px'}
-          imageHeight={'240px'}
-          imageWidth={'240px'}
-        />
-        <spacer size="small" />
         <text maxWidth={`80%`} size="large" weight="bold" alignment="center middle" wrap>
           {text}
         </text>
